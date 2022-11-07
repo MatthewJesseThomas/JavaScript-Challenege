50dc84c702c547e2c2a2cdcfdde15f5f2740ec55
MatthewJesseThomas <owlsaintgermain@gmail.com>
2022-11-07 14:56:48 +0200

    main

--- js/code.js
+++ js/code.js
@@ -15,3 +15,16 @@ let isOld = true;
 // let output = `Message: ` +  message + `<br>Age: ` + age + `<br>Old: `+ isOld;
 let output = `Message: ${message}<br>Age: ${age} <br>Old: ${isOld}`
 document.write(output);
+// Object and its properties //
+// let john = {
+//     name: "John",
+//     age: 19,
+//     isMale: true,
+//   }
+//   document.write(`Hello, I am ${john.name}, I am ${john.age} years old. If you think I am male, that is ${john.isMale}.`)
+// Create Array //
+let names = ['Micheal','Raphael','Gabriel','Samiel']
+console.log(names); 
+let archAngel = names
+document.write('<br>' + archAngel);
+// End Of Array //
