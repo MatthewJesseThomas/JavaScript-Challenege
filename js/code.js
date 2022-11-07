7b996ded95591c8703dc2d41e6b9f94e4286d510
MatthewJesseThomas <owlsaintgermain@gmail.com>
2022-11-07 14:35:31 +0200

    main

---
+++ js/code.js
@@ -0,0 +1,17 @@
+// let message = 'Deviant';
+// document.write(message + '<br>');
+
+// let age = 43;
+// document.write(age + '<br>');
+
+// let isOld = true;
+// document.write(isOld);
+
+let message = 'Deviant';
+let age = 43;
+let isOld = true;
+
+
+// let output = `Message: ` +  message + `<br>Age: ` + age + `<br>Old: `+ isOld;
+let output = `Message: ${message}<br>Age: ${age} <br>Old: ${isOld}`
+document.write(output);
